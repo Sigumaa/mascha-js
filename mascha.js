db010d24e1ee804ceab4850accb91d58dfed36c6
Sigumaa <66453922+Sigumaa@users.noreply.github.com>
2022-12-27 17:11:54 +0900

    feat: send

--- mascha.js
+++ mascha.js
@@ -1,4 +1,10 @@
 const readline = require('readline');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+server = process.env.SERVER;
+token = process.env.TOKEN;
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -7,13 +13,13 @@ const rl = readline.createInterface({
 
 rl.question('文字列を入力してください: ', (str) => {
   console.log(`入力された文字列: ${str}`);
-  main(str);
+  send(str);
   rl.close();
 });
 
 async function sendRequest(c) {
   await new Promise(resolve => setTimeout(resolve, 1000));
-  return fetch("https://mstdn.jp/api/v1/statuses?access_token=", {
+  return fetch(server+token, {
     method: "POST",
     headers: {
         "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
@@ -25,8 +31,14 @@ async function sendRequest(c) {
   .catch(err => console.error(err));
 }
 
-async function main(str) {
-    for (let i = str.length - 1; i >= 0; i--) {
-      await sendRequest(str[i]);
+is_normal = false;
+
+async function send(str) {
+    if (is_normal) {
+        for (let i = str.length - 1; i >= 0; i--) {
+            await sendRequest(str[i]);
+        }
+    } else {
+        await sendRequest(str);
     }
 }
